5810c81944ee744c86d857f7fc406a16a57df7f2
lmodin <lisamodin@live.com>
2020-06-03 19:32:34 -0700

    Updating with our tables for messages, rooms, users. Command line showing tables correctly. Foreign ids are referencing correctly. No friends table implemented at this time

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,47 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE messages (
+  /* list of all the messages in the application */
+  id INTEGER AUTO_INCREMENT,
+  text VARCHAR(140) NOT NULL,
+  room_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (room_id)
+    REFERENCES rooms(id),
+  FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
+/* Create other tables and define schemas for them here!
+    FOREIGN KEY (parent_id)
+      REFERENCES parent(id)
+*/
+
+
+
+
+
+/*ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms (id);
+ALTER TABLE messages ADD FOREIGN KEY (user_id) REFERENCES users (id);*/
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
